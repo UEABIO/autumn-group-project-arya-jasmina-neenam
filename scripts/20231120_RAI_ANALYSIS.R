62fac82d1dbf9e8a7e39d4d5156d111712997805
xws22wmu <xws22wmu@uea.ac.uk>
2023-12-11 12:32:35 +0000

    2nd commit

---
+++ scripts/20231120_RAI_ANALYSIS.R
@@ -0,0 +1,10 @@
+###-------SUMMATIVE ASSIGNMENT------
+##------NEENAM RAI INDIVIDUAL ANALYSIS----
+#20/11/2023
+
+##---PACKAGES--
+library(tidyverse)
+library(janitor)
+library(lubridate)
+
+##---CLEAN DATA--
