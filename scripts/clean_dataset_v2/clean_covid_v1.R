fb3c38ba75b113576bbaac05a1c8dd2f0e3f1bfa
xws22wmu <xws22wmu@uea.ac.uk>
2023-12-21 08:52:43 +0000

    error installing dplyr package fixed

--- scripts/clean_dataset_v2/clean_covid_v1.R
+++ scripts/clean_dataset_v2/clean_covid_v1.R
@@ -11,24 +11,11 @@ library(grid)
 # IMPORT DATA ----
 covid <-read_csv("data/covid_example_data (1).csv")
 head(covid)
-
 #__________________________----
 #CLEAN DATA----
 covid <- janitor::clean_names(covid) # clean the column names
 
 colnames(covid) # check the new variable names
-
-covid <- rename(covid,
-                "date_of_entry_for_case"="reprt_creationdt_false",
-                "date_of_birth"="case_dob_false",
-                "symptomatic_status"="contact_id" ,
-                "date_of_first_symptoms"="sym_startdt_false",
-                "symptoms_now_resolved"="sym_resolveddt_false",
-                "date_of_hospital_admission"="hosp_admidt_false",
-                "date_of_hospital_discharge"="hosp_dischdt_false",
-                "date_of_death"= "died_dt_false",
-                "date of positive PCR test" = "pos_sampledt_false",
-                "case_ethnicity" = "case_eth") #re-names columns to easily understandable text
 
 
 #glimpse(covid) # displays column names in console
