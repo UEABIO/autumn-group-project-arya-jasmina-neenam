830f2a17778c3703b679cddedfdb9ff072a89593
xws22wmu <xws22wmu@uea.ac.uk>
2023-12-21 08:53:00 +0000

    error fixed

--- scripts/clean_dataset_v2/clean_covid_v1.R
+++ scripts/clean_dataset_v2/clean_covid_v1.R
@@ -17,11 +17,20 @@ covid <- janitor::clean_names(covid) # clean the column names
 
 colnames(covid) # check the new variable names
 
+ovid <- rename(covid,
+               "date_of_entry_for_case"="reprt_creationdt_false",
+               "date_of_birth"="case_dob_false",
+               "symptomatic_status"="contact_id" ,
+               "date_of_first_symptoms"="sym_startdt_false",
+               "symptoms_now_resolved"="sym_resolveddt_false",
+               "date_of_hospital_admission"="hosp_admidt_false",
+               "date_of_hospital_discharge"="hosp_dischdt_false",
+               "date_of_death"= "died_dt_false",
+               "date of positive PCR test" = "pos_sampledt_false",
+               "case_ethnicity" = "case_eth") #re-names columns to easily understandable text
 
-#glimpse(covid) # displays column names in console
-
+glimpse(covid) # displays column names in console
 colnames(covid) # check for duplicate rows in the data
-
 
 duplicated(covid)# produces a list of TRUE/FALSE statements for duplicated or not
 sum() # sums all the TRUE statements                                                                                                                                
