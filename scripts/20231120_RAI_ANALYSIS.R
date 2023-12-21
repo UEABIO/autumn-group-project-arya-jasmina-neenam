c77b073249821787f3de5526fe5ab661c3441259
xws22wmu <xws22wmu@uea.ac.uk>
2023-12-21 00:59:29 +0000

    cleaned up the annotations

--- scripts/20231120_RAI_ANALYSIS.R
+++ scripts/20231120_RAI_ANALYSIS.R
@@ -1,8 +1,8 @@
 
 # PACKAGES ----
-library(tidyverse) # tidy data packages
-library(janitor) # cleans variable names
-library(lubridate) # make sure dates are processed properly
+library(tidyverse)
+library(janitor) 
+library(lubridate) 
 library(scales)
 library(patchwork)
 
@@ -10,33 +10,23 @@ library(patchwork)
 #__________________________----
 # IMPORT DATA ----
 covid <- read_csv ("data/owid-covid-data.csv")
-head(covid) # check the data has loaded, prints first 10 rows of dataframe
-
+head(covid)
 
 #__________________________----
 # CHECK DATA----
 # check the data
 colnames(covid)
-
-
-covid <- janitor::clean_names(covid) # clean the column names
-
+covid <- janitor::clean_names(covid) 
 colnames(covid) # quickly check the new variable names
-
-#glimpse(covid) # displays column names in console
-
+glimpse(covid) # displays column names in console
 colnames(covid)
-# check for duplicate rows in the data
 
-
+# check for duplicates---
 duplicated(covid)# produces a list of TRUE/FALSE statements for duplicated or not
 sum() # sums all the TRUE statements                                                                                                                                
-
 covid %>% 
   is.na() %>% #checks for N/A values in data
   sum()
-
 summary(covid) # summaries data
-
 filter(.data = covid, continent %in% c("north america", "south america"))
 
