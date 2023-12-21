38a398d5605895396c02669952f71fbb517fee66
xws22wmu <xws22wmu@uea.ac.uk>
2023-12-21 01:18:57 +0000

    loading a dplyr package

--- scripts/20231120_RAI_ANALYSIS.R
+++ scripts/20231120_RAI_ANALYSIS.R
@@ -5,7 +5,7 @@ library(janitor)
 library(lubridate) 
 library(scales)
 library(patchwork)
-
+library(dplyr)
 
 #__________________________----
 # IMPORT DATA ----
@@ -16,6 +16,18 @@ head(covid)
 # CHECK DATA----
 # check the data
 colnames(covid)
+covid <- rename(covid,
+                "date_of_entry_for_case"="reprt_creationdt_false",
+                "date_of_birth"="case_dob_false",
+                "symptomatic_status"="contact_id" ,
+                "date_of_first_symptoms"="sym_startdt_false",
+                "symptoms_now_resolved"="sym_resolveddt_false",
+                "date_of_hospital_admission"="hosp_admidt_false",
+                "date_of_hospital_discharge"="hosp_dischdt_false",
+                "date_of_death"= "died_dt_false",
+                "date of positive PCR test" = "pos_sampledt_false",
+                "case_ethnicity" = "case_eth")
+
 covid <- janitor::clean_names(covid) 
 colnames(covid) # quickly check the new variable names
 glimpse(covid) # displays column names in console
