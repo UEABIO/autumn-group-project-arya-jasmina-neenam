4b163ae10be0e08900b4c68424fad8d578378080
xws22wmu <xws22wmu@uea.ac.uk>
2023-12-21 08:04:18 +0000

    cleaning dataset covid_example_data first time

---
+++ scripts/clean_dataset_v2/clean_covid_v1.R
@@ -0,0 +1,46 @@
+#LOAD PACKAGES
+library(tidyverse)
+library(janitor)
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+library(maps)
+library(grid)
+
+#___________________________----
+# IMPORT DATA ----
+covid <-read_csv("data/covid_example_data (1).csv")
+head(covid)
+
+#__________________________----
+#CLEAN DATA----
+covid <- janitor::clean_names(covid) # clean the column names
+
+colnames(covid) # check the new variable names
+
+covid <- rename(covid,
+                "date_of_entry_for_case"="reprt_creationdt_false",
+                "date_of_birth"="case_dob_false",
+                "symptomatic_status"="contact_id" ,
+                "date_of_first_symptoms"="sym_startdt_false",
+                "symptoms_now_resolved"="sym_resolveddt_false",
+                "date_of_hospital_admission"="hosp_admidt_false",
+                "date_of_hospital_discharge"="hosp_dischdt_false",
+                "date_of_death"= "died_dt_false",
+                "date of positive PCR test" = "pos_sampledt_false",
+                "case_ethnicity" = "case_eth") #re-names columns to easily understandable text
+
+
+#glimpse(covid) # displays column names in console
+
+colnames(covid) # check for duplicate rows in the data
+
+
+duplicated(covid)# produces a list of TRUE/FALSE statements for duplicated or not
+sum() # sums all the TRUE statements                                                                                                                                
+
+covid %>% 
+  is.na() %>% #checks for N/A values in data
+  sum()
+
+summary(covid) # summaries data
