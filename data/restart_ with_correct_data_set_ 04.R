7990cce0451d7d3f050c01e596052a909dd76ea7
hbs21mhu <hbs21mhu@uea.ac.uk>
2024-01-08 13:39:12 +0000

    created read me file detailing what the folder contains

---
+++ data/restart_ with_correct_data_set_ 04.R
@@ -0,0 +1,104 @@
+#LOAD PACKAGES
+library(tidyverse)
+library(janitor)
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+library(grid)
+library(patchwork)
+
+#___________________________----
+# IMPORT DATA ----
+covid <- read_csv("covid_example_data (1)(1).csv")
+head(covid)
+
+
+#__________________________----
+#CLEAN DATA----
+covid <- janitor::clean_names(covid) # cleans the column names
+
+colnames(covid) # checks the new variable names
+
+covid <- rename(covid,
+                "date_of_entry_for_case"="reprt_creationdt_false",
+                "date_of_birth"="case_dob_false",
+                "symptomatic_status"="contact_id" ,
+                "date_of_first_symptoms"="sym_startdt_false",
+                "symptoms_now_resolved"="sym_resolveddt_false",
+                "date_of_hospital_admission"="hosp_admidt_false",
+                "date_of_hospital_discharge"="hosp_dischdt_false",
+                "date_of_death"= "died_dt_false",
+                "date of positive PCR test" = "pos_sampledt_false",
+                "case_ethnicity" = "case_eth") #re-names columns to easily understandable text
+
+glimpse(covid) # displays column names in console
+
+colnames(covid) # check for duplicate rows in the data
+
+
+duplicated(covid)# produces a list of TRUE/FALSE statements for duplicated or not
+sum() # sums all the TRUE statements   
+
+covid %>% 
+  is.na() %>% #checks for N/A values in data
+  sum()
+
+covid <- covid[!(is.na(covid$case_age)),] # omits NA from case_age data
+
+covid # shows snap shot of data in console
+
+covid <- covid[!(is.na(covid$died_covid)),] #omits NA from died_covid data
+covid <- covid[!(is.na(covid$case_gender)),]#omits NA from case_gender data
+#___________________________----
+#COMPARATIVE GRAPH OF AGE AND GENDER OF PATIENTS STUDIED----
+
+
+pal <- c("tomato2","palegreen3", "lightgrey")
+
+p1 <- covid %>% 
+  ggplot(aes(x = case_gender, # gender displayed on X-axis
+             y = case_age, # age displayed on Y-Axis
+             fill = case_gender,
+             colour = case_gender))+
+  geom_violin(alpha = 0.2)+
+  geom_boxplot(width = 0.2,
+               alpha = 0.7)+ # adds scale
+  scale_fill_manual(values = pal)+ 
+  scale_colour_manual(values = pal)+
+  theme_light()+ # adds in 'light' theme for cohesion between plots
+  theme(legend.position = "none")+
+  labs(
+    x = "pateint gender", #x-axis title
+    y = "patient age", #y-axis title
+    title = "Age verses gender of patients studied", # graph title
+    subtitle = "Box and violin plot of age by gender")# graph sub-title
+
+
+#___________________________----
+#COMPARATIVE GRAPH OF AGE AND DATETO SHOW DEATH RATE----
+
+pal <- c("orange", "lightgrey", "cornflowerblue")
+
+p2 <- covid %>% 
+  ggplot(aes(x = died_covid,# weather they died or not displayed on x-axis
+             y = case_age, # age of patient recorded
+             fill = died_covid,
+             colour = died_covid))+
+  geom_violin(alpha = 0.2)+
+  geom_boxplot(width = 0.2,
+               alpha = 0.7)+ # adds scale
+  scale_fill_manual(values = pal)+
+  scale_colour_manual(values = pal)+
+  theme_light()+ # adds in 'light' theme for cohesion between plots
+  theme(legend.position = "none")+
+  labs(
+    x = "death of patient",
+    y = "patient age",
+    title = "Age of patient death from covid",
+    subtitle = "Box and violin plot of age by death rate") 
+
+#__________________________----
+#COMBINING PLOTS INTO ONE FIGURE----
+
+(p1+p2)+
+plot_layout(guides = "collect") #combines plot 1 and 2 in one figure
---
+++ scripts/jasmina_analysis/jr_read_me.md
@@ -0,0 +1,17 @@
+#Autumn summative project
+
+## What is in my folder?
+
+This folder contains 2 sub-folders:
+
+1\. old_work_with_wrong_data_set_used
+
+2\. new_work_with_correct_data_set_used
+
+### 1. old_work_with_wrong_data_set_used
+
+This folder contains work produced when the wrong data set was used. This folder contains cleaning of the old data, as well as 2 map plots being produced to show 'covid cases per country per million', and 'death rates per country per million', found in the graph_work files. The files have been numbered in ascending order, with the highest number being the most recently edited version.
+
+### 2. new_work_with_correct_data_set_used
+
+This folder has been produced when using the correct data set, and contains all relevant data cleaning and a summertive plot of the patients studied in the data set in order to give context of the data. Again,the files have been numbered in ascending order, with the highest number being the most recently edited version.
